20a897edee11e0ff9274aac82f5c0640d6a3dc12
SpiritCroc <dev@spiritcroc.de>
2020-06-02 12:41:00 +0200

    utils: Import restart systemui api

--- core/java/com/android/internal/util/bootleggers/BootlegUtils.java
+++ core/java/com/android/internal/util/bootleggers/BootlegUtils.java
@@ -19,7 +19,12 @@ package com.android.internal.util.bootleggers;
 import android.app.ActivityManager;
 import android.app.IUiModeManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
+import com.android.internal.R;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -300,6 +305,54 @@ public class BootlegUtils {
         final TypedValue value = new TypedValue ();
         context.getTheme ().resolveAttribute (android.R.attr.colorAccent, value, true);
         return value.data;
+    }
+
+    /**
+     * Restart SystemUI API utils
+     *
+     */
+    public static void restartSystemUi(Context context) {
+        new  RestartSystemUiTask(context).execute();
+    }
+
+    public static void showSystemUiRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSystemUi(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private static class RestartSystemUiTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public RestartSystemUiTask(Context context) {
+            super(); 
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.systemui".equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
     // Omni Switch Constants
